54c5836e1a45fcd6f7342cf6a2dc333551938c4f
Aditya Singh <singhaditya2763@gmail.com>
2022-04-19 21:32:20 +0530

    update

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/shopping-app'
+ const dbUrl = process.env.DB_URL ||'mongodb://localhost:27017/shopping-app'
+
 
 
 mongoose.connect(dbUrl)
